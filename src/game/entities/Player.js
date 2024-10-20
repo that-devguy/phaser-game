c18708acbf2ba075281157faf3f80581d47f8c6d
that devguy <zkmutch@gmail.com>
2024-10-20 11:18:02 -0400

    fixed bug that broke the idle animations after I added the diagonal walking animations

--- src/game/entities/Player.js
+++ src/game/entities/Player.js
@@ -6,7 +6,6 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     scene.add.existing(this);
     scene.physics.add.existing(this);
     this.speed = 160;
-    this.lastDirection = "down"; // Default starting direction
   }
 
   handleMovement(keys) {
@@ -14,30 +13,26 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     this.setVelocity(0);
     let moving = false;
 
-    // Handle Diagonal Movement
+    // Diagonal Movement
     if (keys.w.isDown && keys.a.isDown) {
       this.setVelocityY(-this.speed);
       this.setVelocityX(-this.speed);
       this.anims.play("walk_up", true); // Play up animation for up-left
-      this.lastDirection = "up";
       moving = true;
     } else if (keys.w.isDown && keys.d.isDown) {
       this.setVelocityY(-this.speed);
       this.setVelocityX(this.speed);
       this.anims.play("walk_up", true); // Play up animation for up-right
-      this.lastDirection = "up";
       moving = true;
     } else if (keys.s.isDown && keys.a.isDown) {
       this.setVelocityY(this.speed);
       this.setVelocityX(-this.speed);
       this.anims.play("walk_down", true); // Play down animation for down-left
-      this.lastDirection = "down";
       moving = true;
     } else if (keys.s.isDown && keys.d.isDown) {
       this.setVelocityY(this.speed);
       this.setVelocityX(this.speed);
       this.anims.play("walk_down", true); // Play down animation for down-right
-      this.lastDirection = "down";
       moving = true;
     }
 
@@ -45,12 +40,10 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     if (!moving && keys.a.isDown) {
       this.setVelocityX(-this.speed);
       this.anims.play("walk_left", true); // Play walking left animation
-      this.lastDirection = "left";
       moving = true;
     } else if (!moving && keys.d.isDown) {
       this.setVelocityX(this.speed);
       this.anims.play("walk_right", true); // Play walking right animation
-      this.lastDirection = "right";
       moving = true;
     }
 
@@ -58,30 +51,32 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     if (!moving && keys.w.isDown) {
       this.setVelocityY(-this.speed);
       this.anims.play("walk_up", true); // Play walking up animation
-      this.lastDirection = "up";
       moving = true;
     } else if (!moving && keys.s.isDown) {
       this.setVelocityY(this.speed);
       this.anims.play("walk_down", true); // Play walking down animation
-      this.lastDirection = "down";
       moving = true;
     }
 
     // If no movement, play idle animation
     if (!moving) {
-      switch (this.lastDirection) {
-        case "left":
-          this.anims.play("idle_left");
-          break;
-        case "right":
-          this.anims.play("idle_right");
-          break;
-        case "up":
-          this.anims.play("idle_up");
-          break;
-        case "down":
-          this.anims.play("idle_down");
-          break;
+      // Check last movement direction to play the corresponding idle animation
+      const currentAnim = this.anims.currentAnim;
+      if (currentAnim) {
+        switch (currentAnim.key) {
+          case "walk_left":
+            this.anims.play("idle_left");
+            break;
+          case "walk_right":
+            this.anims.play("idle_right");
+            break;
+          case "walk_up":
+            this.anims.play("idle_up");
+            break;
+          case "walk_down":
+            this.anims.play("idle_down");
+            break;
+        }
       }
     }
 
